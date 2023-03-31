46bd6726ae02aaad624a17d10f353262a1f4c436
Kenneth Doan <ken@kens-mbp.local>
2023-03-31 12:02:10 -0700

    vite test, react testing, and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import { HashRouter, Route, Routes } from 'react-router-dom';
+// broswer router is another thing to try? look at later
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+export function App() {
   return (
-    <div className="App">
-      Hellow World!
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
-
-export default App;
+export function WrappedApp() {
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+}
