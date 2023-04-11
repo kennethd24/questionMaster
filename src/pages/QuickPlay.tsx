1e29e442622ebe65fef1585fb2881261a8306599
Kenneth Doan <kennethxdoan@gmail.com>
2023-04-10 18:06:11 -0700

    New set of questions every start game

--- src/pages/QuickPlay.tsx
+++ src/pages/QuickPlay.tsx
@@ -1,8 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function QuickPlay() {
   const [currentName, setCurrentName] = useState('');
+  const [currentDifficulty, setCurrentDifficulty] = useState('');
+  const [currentCategories, setCurrentCategories] = useState('');
+  const [currentQuestions, setCurrentQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   let name = '';
 
   if (!currentName) {
@@ -10,6 +16,61 @@ export default function QuickPlay() {
   } else {
     name = currentName;
   }
+
+  const categories = [
+    { label: 'Arts & Literature', value: 'arts_and_literature' },
+    { label: 'Film & TV', value: 'film_and_tv' },
+    { label: 'Food & Drink', value: 'food_and_drink' },
+    { label: 'General Knowledge', value: 'general_knowledge' },
+    { label: 'Geography', value: 'geography' },
+    { label: 'History', value: 'history' },
+    { label: 'Music', value: 'music' },
+    { label: 'Science', value: 'science' },
+    { label: 'Society & Culture', value: 'society_and_culture' },
+    { label: 'Sport & Leisure', value: 'sport_and_leisure' },
+  ];
+
+  const difficulty = [
+    { label: 'Easy', value: 'easy' },
+    { label: 'Medium', value: 'medium' },
+    { label: 'Hard', value: 'hard' },
+  ];
+
+  async function handleClick() {
+    // fetch questions from api
+    try {
+      setLoading(true);
+      const url = 'https://the-trivia-api.com/api/questions?limit=10';
+      const response = await fetch(url);
+      const questions = await response.json();
+
+      setCurrentQuestions(questions);
+      setLoading(false);
+      navigate(`/StartGame/${name}`, { state: questions });
+      // navigate to start game with new set of questions/names/etc.
+    } catch (error: unknown) {
+      // navigate to home page with error message
+      console.error(`Unable to fetch questions: ${error.message}`);
+    }
+  }
+
+  /*
+  https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge,geography,history,music,science,society_and_culture,sport_and_leisure&limit=10&difficulty=medium
+  default 10 question api
+  https://the-trivia-api.com/api/questions?limit=10
+
+  arts
+  https://the-trivia-api.com/api/questions?categories=arts_and_literature&limit=10
+
+  arts, film, and food
+  https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink&limit=10
+
+  arts, film, food, and general
+  https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge&limit=10
+
+  arrts
+  https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge&limit=10&difficulty=medium
+  */
 
   return (
     <>
@@ -27,8 +88,40 @@ export default function QuickPlay() {
             placeholder="Enter Name"
           />
         </div>
-        <div>Categories</div>
-        <div>Difficulty</div>
+        <div>
+          <select name="categories">
+            <option value="">Categories</option>
+            {categories.map((item) => (
+              <option
+                value={item.label}
+                key={item.label}
+                onClick={() => setCurrentCategories(item.value)}
+              >
+                {item.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <select name="difficulty">
+            <option value="">Difficulty</option>
+            {difficulty.map((item) => (
+              <option
+                value={item.label}
+                key={item.label}
+                onClick={() => setCurrentDifficulty(item.value)}
+              >
+                {item.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <Link to={`/StartGame/${name}`}>Start Game</Link>
+          <button type="submit" onClick={handleClick}>
+            Start Game
+          </button>
+        </div>
         <div>
           <Link to={`/StartGame/${name}`}>Start Game</Link>
         </div>
--- src/pages/StartGame.tsx
+++ src/pages/StartGame.tsx
@@ -1,150 +1,169 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, useNavigate, useParams, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function StartGame() {
-  const sampleQuestion = [
-    {
-      category: 'Film & TV',
-      id: '622a1c377cc59eab6f950701',
-      correctAnswer: 'Sandra Bullock',
-      incorrectAnswers: ['Charlize Theron', 'Emma Thompson', 'Maggie Smith'],
-      question:
-        'Which actress has featured in films including Crash and Speed?',
-      tags: ['film_and_tv'],
-      type: 'Multiple Choice',
-      difficulty: 'hard',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Geography',
-      id: '622a1c377cc59eab6f9507ed',
-      correctAnswer: 'North America',
-      incorrectAnswers: ['South America', 'Oceania', 'Europe'],
-      question: 'The country of Grenada is on which continent?',
-      tags: ['geography'],
-      type: 'Multiple Choice',
-      difficulty: 'medium',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Arts & Literature',
-      id: '622a1c347cc59eab6f94f970',
-      correctAnswer: 'On the Road',
-      incorrectAnswers: [
-        'The Member of the Wedding',
-        '1984',
-        "Breakfast at Tiffany's",
-      ],
-      question: "Which book contains the character 'Dean Moriarty'?",
-      tags: [
-        'literature',
-        'classic_novels',
-        'fictitious_characters',
-        'arts_and_literature',
-      ],
-      type: 'Multiple Choice',
-      difficulty: 'hard',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Music',
-      id: '625063f9e12f6dec240bdf90',
-      correctAnswer: 'It Takes Two',
-      incorrectAnswers: ['I Touch Myself', 'How Bizarre', 'Whip It'],
-      question: 'What song did Rob Base & DJ E-Z Rock have a hit with in 1988?',
-      tags: ['songs', 'one_hit_wonders', 'music'],
-      type: 'Multiple Choice',
-      difficulty: 'hard',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Sport & Leisure',
-      id: '622a1c377cc59eab6f95045b',
-      correctAnswer: 'Pong',
-      incorrectAnswers: ['Space Invaders', 'Pacman', 'Tetris'],
-      question: 'What arcade game became a hit in 1973?',
-      tags: ['technology', 'video_games', 'sport'],
-      type: 'Multiple Choice',
-      difficulty: 'hard',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Arts & Literature',
-      id: '622a1c3a7cc59eab6f951387',
-      correctAnswer: 'Harry Potter',
-      incorrectAnswers: [
-        'Voyages Extraordinaires',
-        'Percy Jackson & the Olympians',
-        'Twilight',
-      ],
-      question: "In which book series does 'Cedric Diggory' appear?",
-      tags: ['arts_and_literature'],
-      type: 'Multiple Choice',
-      difficulty: 'medium',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Food & Drink',
-      id: '624c60e050d1a5e051325a79',
-      correctAnswer: 'Central America',
-      incorrectAnswers: ['Brunei', 'Chile', 'The Middle East'],
-      question:
-        'Where in the world would you most expect to be served Gallo Pinto?',
-      tags: ['food', 'food_and_drink'],
-      type: 'Multiple Choice',
-      difficulty: 'hard',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'History',
-      id: '622a1c3c7cc59eab6f9517ac',
-      correctAnswer: 'Davy Crockett',
-      incorrectAnswers: ['Davy Jones', 'Davy Barrow', 'Davy Dylan'],
-      question: 'Which frontiersman died at the Alamo?',
-      tags: ['usa', 'history'],
-      type: 'Multiple Choice',
-      difficulty: 'medium',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Music',
-      id: '622a1c357cc59eab6f94feac',
-      correctAnswer: 'Elton John',
-      incorrectAnswers: ['Mika', 'Madonna', 'Eric Clapton'],
-      question: "Which musician released 'Your Song'?",
-      tags: ['songs', 'musicians', 'general_knowledge', 'music'],
-      type: 'Multiple Choice',
-      difficulty: 'medium',
-      regions: [],
-      isNiche: false,
-    },
-    {
-      category: 'Sport & Leisure',
-      id: '622a1c367cc59eab6f950027',
-      correctAnswer: 'Golf',
-      incorrectAnswers: ['Tennis', 'Cricket', 'Rugby'],
-      question: 'With what sport is Jack Nicklaus associated?',
-      tags: ['people', 'general_knowledge', 'sport'],
-      type: 'Multiple Choice',
-      difficulty: 'easy',
-      regions: [],
-      isNiche: false,
-    },
-  ];
+  // questions: [
+  //   {
+  //     category: string;
+  //     id: string;
+  //     correctAnswer: string;
+  //     incorrectAnswers: string[];
+  //     question: string;
+  //     tags: string[];
+  //     type: string;
+  //     difficulty: string;
+  //     regions: string[];
+  //     isNiche: boolean;
+  //   }
+  // ]
+  // const { questions } = props;
+
+  // const questions = [
+  //   {
+  //     category: 'Film & TV',
+  //     id: '622a1c377cc59eab6f950701',
+  //     correctAnswer: 'Sandra Bullock',
+  //     incorrectAnswers: ['Charlize Theron', 'Emma Thompson', 'Maggie Smith'],
+  //     question:
+  //       'Which actress has featured in films including Crash and Speed?',
+  //     tags: ['film_and_tv'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'hard',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Geography',
+  //     id: '622a1c377cc59eab6f9507ed',
+  //     correctAnswer: 'North America',
+  //     incorrectAnswers: ['South America', 'Oceania', 'Europe'],
+  //     question: 'The country of Grenada is on which continent?',
+  //     tags: ['geography'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'medium',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Arts & Literature',
+  //     id: '622a1c347cc59eab6f94f970',
+  //     correctAnswer: 'On the Road',
+  //     incorrectAnswers: [
+  //       'The Member of the Wedding',
+  //       '1984',
+  //       "Breakfast at Tiffany's",
+  //     ],
+  //     question: "Which book contains the character 'Dean Moriarty'?",
+  //     tags: [
+  //       'literature',
+  //       'classic_novels',
+  //       'fictitious_characters',
+  //       'arts_and_literature',
+  //     ],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'hard',
+  //     regions: string[],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Music',
+  //     id: '625063f9e12f6dec240bdf90',
+  //     correctAnswer: 'It Takes Two',
+  //     incorrectAnswers: ['I Touch Myself', 'How Bizarre', 'Whip It'],
+  //     question: 'What song did Rob Base & DJ E-Z Rock have a hit with in 1988?',
+  //     tags: ['songs', 'one_hit_wonders', 'music'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'hard',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Sport & Leisure',
+  //     id: '622a1c377cc59eab6f95045b',
+  //     correctAnswer: 'Pong',
+  //     incorrectAnswers: ['Space Invaders', 'Pacman', 'Tetris'],
+  //     question: 'What arcade game became a hit in 1973?',
+  //     tags: ['technology', 'video_games', 'sport'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'hard',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Arts & Literature',
+  //     id: '622a1c3a7cc59eab6f951387',
+  //     correctAnswer: 'Harry Potter',
+  //     incorrectAnswers: [
+  //       'Voyages Extraordinaires',
+  //       'Percy Jackson & the Olympians',
+  //       'Twilight',
+  //     ],
+  //     question: "In which book series does 'Cedric Diggory' appear?",
+  //     tags: ['arts_and_literature'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'medium',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Food & Drink',
+  //     id: '624c60e050d1a5e051325a79',
+  //     correctAnswer: 'Central America',
+  //     incorrectAnswers: ['Brunei', 'Chile', 'The Middle East'],
+  //     question:
+  //       'Where in the world would you most expect to be served Gallo Pinto?',
+  //     tags: ['food', 'food_and_drink'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'hard',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'History',
+  //     id: '622a1c3c7cc59eab6f9517ac',
+  //     correctAnswer: 'Davy Crockett',
+  //     incorrectAnswers: ['Davy Jones', 'Davy Barrow', 'Davy Dylan'],
+  //     question: 'Which frontiersman died at the Alamo?',
+  //     tags: ['usa', 'history'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'medium',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Music',
+  //     id: '622a1c357cc59eab6f94feac',
+  //     correctAnswer: 'Elton John',
+  //     incorrectAnswers: ['Mika', 'Madonna', 'Eric Clapton'],
+  //     question: "Which musician released 'Your Song'?",
+  //     tags: ['songs', 'musicians', 'general_knowledge', 'music'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'medium',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  //   {
+  //     category: 'Sport & Leisure',
+  //     id: '622a1c367cc59eab6f950027',
+  //     correctAnswer: 'Golf',
+  //     incorrectAnswers: ['Tennis', 'Cricket', 'Rugby'],
+  //     question: 'With what sport is Jack Nicklaus associated?',
+  //     tags: ['people', 'general_knowledge', 'sport'],
+  //     type: 'Multiple Choice',
+  //     difficulty: 'easy',
+  //     regions: [],
+  //     isNiche: false,
+  //   },
+  // ];
 
   const { name } = useParams();
   const [questionIndex, setQuestionIndex] = useState(0);
   const [rightAnswers, setRightAnswers] = useState(0);
+  const { state } = useLocation();
+  const questions = state;
+
   const { question, correctAnswer, incorrectAnswers } =
-    sampleQuestion[questionIndex];
+    questions[questionIndex];
 
   const allAnswers: string[] = [correctAnswer].concat(incorrectAnswers);
   const randomOrder: string[] = allAnswers.sort(() => 0.5 - Math.random());
@@ -161,7 +180,7 @@ export default function StartGame() {
     }
     setQuestionIndex(questionIndex + 1);
 
-    if (questionIndex === sampleQuestion.length - 1) {
+    if (questionIndex === questions.length - 1) {
       navigate(`/Results/${rightAnswers}/${name}`);
     }
   }
@@ -174,7 +193,7 @@ export default function StartGame() {
         </div>
         <div className="banner">
           <h6>
-            {questionIndex + 1}/{sampleQuestion.length}
+            {questionIndex + 1}/{questions.length}
           </h6>
           <h6>Countdown Timer</h6>
           <h6>Score: {rightAnswers}</h6>
