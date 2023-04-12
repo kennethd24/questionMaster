d409a940ff5c81cc4cf92b3088a791c8736d0d7d
Kenneth Doan <kennethxdoan@gmail.com>
2023-04-12 16:07:14 -0700

    Altered css for views

--- src/pages/StartGame.tsx
+++ src/pages/StartGame.tsx
@@ -88,6 +88,9 @@ export default function StartGame() {
             type="button"
             id={answer}
             key={answer}
+            style={{
+              minWidth: '300px',
+            }}
             onClick={() => verifyAnswer(answer)}
           >
             {answer}
