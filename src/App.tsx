a9b4a69f516db380e3b4c9d28b01ab279e009f4b
Kenneth Doan <kennethxdoan@gmail.com>
2023-04-13 16:56:30 -0700

    Added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// broswer router is another thing to try? look at later
+import { initThinBackend, logout } from 'thin-backend';
+import { useCurrentUser, ThinBackend } from 'thin-backend-react';
 import Home from './pages/Home';
 import QuickPlay from './pages/QuickPlay';
 import StartGame from './pages/StartGame';
 import NotFound from './pages/NotFound';
 import Results from './pages/Results';
 
+import './thinBackend.css';
+
+initThinBackend({
+  host: 'https://questionmaster.thinbackend.app',
+});
+
 export function App() {
+  const user = useCurrentUser();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/QuickPlay" element={<QuickPlay />} />
-      <Route path="/StartGame/:name" element={<StartGame />} />
-      <Route path="/Results" element={<Results />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <ThinBackend requireLogin>
+      <div className="header">
+        <b>{user?.email}</b>
+        <button type="button" onClick={logout}>
+          Logout
+        </button>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/QuickPlay" element={<QuickPlay />} />
+        <Route path="/StartGame/:name" element={<StartGame />} />
+        <Route path="/Results" element={<Results />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ThinBackend>
   );
 }
 export function WrappedApp() {
